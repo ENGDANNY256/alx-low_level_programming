b3b28fe9debf3670b7b62371e741db0f3208ff74
ENG DANNY 256 <engdanny256@gmail.com>
2022-12-13 05:54:56 -0800

    task 1 & 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,8 +14,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (lettter = 'a'; letter <= 'z'; letter++)
-			_put char (letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
