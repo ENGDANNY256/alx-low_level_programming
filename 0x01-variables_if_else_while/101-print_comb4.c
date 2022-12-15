177e1bb208f87c0cae95919d2b1c8c9ff6b78444
ENG DANNY 256 <engdanny256@gmail.com>
2022-12-14 18:01:55 -0800

    tt

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations
- * of three digits
- * Return: 0
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int i;
-	int n;
-	int j;
+	int digit1, digit2, digit3;
 
-
-	for (i = 48; i < 58; i++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (n = 48; n < 58; n++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			if (n > i)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				for (j = 48; j < 58; j++)
-				{
-					if (j > n)
-					{
-						putchar(i);
-						putchar(n);
-						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
-						{
-							break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
-
-
